85ef9e2d2bba3770a935a5d932e1edf46c540f58
rafaelgonzalez <rafael.glez@gmail.com>
2024-05-04 11:58:38 -0600

    Initial Commit

---
+++ WSCRUD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WSCRUD
+//
+//  Created by Rafael González on 04/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
