5ee6a5b17995123408f2c37eb35c6c0a5cdabead
rafaelgonzalez <rafael.glez@gmail.com>
2024-05-05 07:15:59 -0600

    Initial version

---
+++ WSCRUD/Service/PostServiceManager.swift
@@ -0,0 +1,107 @@
+//
+//  PostServiceManager.swift
+//  WSCRUD
+//
+//  Created by Rafael González on 04/05/24.
+//
+
+import Foundation
+
+class PostServiceManager {
+    
+    static let shared = PostServiceManager()
+    init(){}
+    
+    //MARK: Create Method
+    func createPost(post : Post, completion: @escaping (Post?) -> Void ) {
+        
+        guard let url = URL(string: Constants.apiDomain+Constants.postURL) else {
+            completion(nil)
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type") //value - key
+        
+        do {
+            let jsonData = try JSONEncoder().encode(post)
+            request.httpBody = jsonData
+            
+            let task = URLSession.shared.dataTask(with: request){(data, response, error) in
+                
+                if let data = data {
+                    if let createdPost = try? JSONDecoder().decode(Post.self, from: data){
+                        completion(createdPost)
+                    }
+                }
+                else if let error = error {
+                    print("Error: ", error)
+                    completion(nil)
+                }
+            }
+            task.resume()
+        }
+        catch let error {
+            print("Error: ", error)
+            completion(nil)
+        }
+    }
+    
+    //MARK: Update Method
+    func updatePost(post : Post, completion: @escaping (Post?) -> Void ) {
+        
+        guard let url = URL(string: Constants.apiDomain+Constants.postURL+String(post.id!) ) else {
+            completion(nil)
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type") //value - key
+        
+        do {
+            let jsonData = try JSONEncoder().encode(post)
+            request.httpBody = jsonData
+            
+            let task = URLSession.shared.dataTask(with: request){(data, response, error) in
+                
+                if let data = data {
+                    if let updatedPost = try? JSONDecoder().decode(Post.self, from: data){
+                        completion(updatedPost)
+                    }
+                }
+                else if let error = error {
+                    print("Error: ", error)
+                    completion(nil)
+                }
+            }
+            task.resume()
+        }
+        catch let error {
+            print("Error: ", error)
+            completion(nil)
+        }
+    }
+    
+    
+    //MARK: Delete Method
+    func deletePost(id: Int, completion: @escaping (Int) -> Void ) {
+        let url = URL(string: Constants.apiDomain+Constants.postURL+String(id))!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                completion(response.statusCode)
+            }
+            else if let error = error {
+                print("Error: ", error)
+                completion(0)
+            }
+        }
+        task.resume()
+    }
+    
+}
--- WSCRUD/ViewController.swift
+++ WSCRUD/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let postService = PostServiceManager()
+    var newPost = Post(id: nil, title: "Hello", body: "API!!", userId: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        //MARK: Create
+        postService.createPost(post: newPost){ createdPost in
+            if let createdPost = createdPost {
+                print("created post: ", createdPost)
+            }
+            else {
+                print("Error: Failed to create post")
+            }
+        }
+        
+        //MARK: Update
+        let myPost = Post(id: 5, title: "Updated post!!", body: "New information", userId: 5)
+        postService.updatePost(post: myPost){ updatedPost in
+            if let updatedPost = updatedPost {
+                print("updated post: ", updatedPost)
+            }
+            else {
+                print("Error: Failed to updated post")
+            }
+        }
+        
+        //MARK: Delete
+        postService.deletePost(id: 5){ value in
+            if value == 200 {
+                print("Post deleted!")
+            }
+            else {
+                print("Error: Failed to deleted post!")
+            }
+        }
     }
-
-
 }
 
